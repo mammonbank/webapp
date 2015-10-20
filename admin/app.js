15c4e84683e7e5209ea72d3557211d5564625d01
Sergey Kazakov <tocher@yandex-team.ru>
2015-10-21 00:24:23 +0300

    admin level. Правка кодстайла

--- admin/app.js
+++ admin/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var express = require('express');
-var path = require('path');
-var app = express();
+var express = require('express'),
+    path = require('path'),
+    app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- admin/controllers/index.js
+++ admin/controllers/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router();
 
 router.get('/', function(req, res) {
     res.render('index', { message: 'Hello, admin!' });
