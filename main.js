b77e2cd5de6f30b540a0607e56912a700bd576de
Sergey Kazakov <tocher@yandex-team.ru>
2015-10-21 00:23:02 +0300

    Правки на общем уровне

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 'use strict';
 
-var shelljs = require('shelljs');
-var env = process.env.NODE_ENV || 'development';
+var shelljs = require('shelljs'),
+    env = process.env.NODE_ENV || 'development';
 
 shelljs.echo('App is starting in ' + env + ' environment');
 
-if ( env === 'production' ) {
+if (env === 'production') {
     shelljs.exec('pm2 start client/bin/www -i 0 --name "client"');
     shelljs.exec('pm2 start admin/bin/www -i 0 --name "admin"');
-}
-else {
+} else {
     shelljs.exec('pm2 start client/bin/www -i 1 --name "client" --watch');
     shelljs.exec('pm2 start admin/bin/www -i 1 --name "admin" --watch');
 }
