9dc79318b4e3274c8feae3bd5215b466804211be
wiranoid <wiranoid@gmail.com>
2015-12-06 07:35:07 -0500

    Launching changes

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ shelljs.echo('App is starting in ' + env + ' environment');
 
 if (env === 'production') {
     shelljs.exec('pm2 start api/bin/www -i 0 --name "api"');
-    shelljs.exec('pm2 start client/bin/www -i 0 --name "client"');
-    shelljs.exec('pm2 start bank/bin/www -i 0 --name "bank"');
+    shelljs.exec('cd bem && pm2 start client -i 0 --name "client"');
+    shelljs.exec('cd bem && pm2 start bank -i 0 --name "bank"');
 } else {
     shelljs.exec('pm2 start api/bin/www -i 1 --name "api"');
-    shelljs.exec('pm2 start client/bin/www -i 1 --name "client" --watch');
-    shelljs.exec('pm2 start bank/bin/www -i 1 --name "bank" --watch');
+    shelljs.exec('cd bem && pm2 start client -i 1 --name "client"');
+    shelljs.exec('cd bem && pm2 start bank -i 1 --name "bank"');
 }
