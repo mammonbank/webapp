b122e9c1aa1383328be3f708658e0c5f8179969f
Tocher <tocher99@gmail.com>
2015-12-29 04:04:27 +0300

    fix url

--- client/bem/desktop.blocks/info-bar/info-bar.js
+++ client/bem/desktop.blocks/info-bar/info-bar.js
@@ -71,7 +71,7 @@ provide(BEMDOM.decl('info-bar', {
         this.elem('status').html('<b>Статус аккаунта:</b> ' + stat);
 
         mLeft.findBlockInside('link')
-            .setUrl('http://mammonwebapi.azurewebsites.net/?clientLink=' + data.scoringFormId);
+            .setUrl(data.scoringFormId);
 
         if (!data.isConfirmed) {
             mLeft.setMod('user', 'inactive');
